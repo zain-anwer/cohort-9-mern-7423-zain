0415e5c9ca3ee107a40c83cca86cbb334afa1816
zain <zain.anwer192005@gmail.com>
2026-08-17 06:07:53 +0500

    fix: extend routing to include base URL and incorrect URls

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import { Routes , Route } from 'react-router-dom'
+import { Routes , Route, Navigate } from 'react-router-dom'
 import { SignupPage } from './pages/SignupPage.jsx'
 import { SigninPage } from './pages/SigninPage.jsx'
 import { Dashboard } from './pages/Dashboard.jsx'
 import { NoteEditor } from './pages/NoteEditor.jsx'
 import { UserProfile } from './pages/UserProfile.jsx'
+import { NotFoundPage } from './pages/NotFoundPage.jsx'
 import { ProtectedRoute } from './components/ProtectedRoute.jsx'
 
 /* using routes and route components we define paths to different pages */
@@ -15,9 +16,13 @@ const App = () => {
       <Route path='/signin' element={<SigninPage/>}/>
       <Route element={<ProtectedRoute/>}>
         <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/dashboard/notes/:note-id' element={<NoteEditor/>}/>
+        <Route path='/dashboard/notes/:id' element={<NoteEditor/>}/>
+        
+        /* adding replace swaps history of navigating to '/' with '/dashboard' */
+        <Route path='/' element={<Navigate to='/dashboard' replace/>}/>
         <Route path='/profile' element={<UserProfile/>}/>
       </Route>
+      <Route path='*' element={<NotFoundPage/>}/>
     </Routes>
   )
 }
